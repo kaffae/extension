afa8b124965c627ee96d602f6628eb47bce65498
Masatoshi Nishimura <massa_n@yahoo.ca>
2018-10-25 14:06:56 -0400

    Support reuters

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 
 
 function checkUrlMatch(url) {
-  const urlSupported = ['https://www.technologyreview.com'];
+  const urlSupported = ['https://www.technologyreview.com', 'https://www.reuters.com'];
   if (!urlSupported.some(supported => url.indexOf(supported) === 0)) return false;
 
   // URL check to make sure it is individual article.
