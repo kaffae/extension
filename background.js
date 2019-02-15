6fec30cfb244ebe5da3f77850f1d820f027f7c21
Masatoshi Nishimura <massa_n@yahoo.ca>
2019-02-15 11:56:02 -0500

    Clean up comments

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ function checkUrlMatch(url) {
   // URL check to make sure it is individual article.
   // Check for the last bit of url path. Ex) https://www.technologyreview.com/s/612276/your-genome-on-demand/
   // Conditions:
-  // 1. Hyphen separated words consist of more than 3 words.
+  // 1. Hyphen separated words consist of more than 3 words. Use that as a marker for blog/news site.
   // 2. Only for Wikipedia, allow any kind of article page since many articles can be a single title.
 
   // Remove hash (comes after query, ?), and query.
@@ -28,15 +28,16 @@ function checkUrlMatch(url) {
 
   const lastPath = paths[paths.length - 1].length === 0 ? paths[paths.length - 2] : paths[paths.length - 1];
 
-  // Only skip wikipedia specific catalogue page like Category or Portal.
+  // Only skip the url test on wikipedia domain.
   if (url.indexOf('https://en.wikipedia.org/wiki') === 0) {
+    // Ignore wikipedia specific catalogue page like Category or Portal that is separated by colon.
     if (lastPath.match(/:/)) {
       return false;
     }
     return true;
   }
 
-  // Ex) https://www.technologyreview.com/s/612021/advanced-tech-but-growth-slow-and-unequal-paradoxes-and-policies/?set=535821
+  // Ex url) https://www.technologyreview.com/s/612021/advanced-tech-but-growth-slow-and-unequal-paradoxes-and-policies
   if (lastPath.split('-').length < 3) {
     return false;
   }
@@ -67,6 +68,10 @@ function saveUrl(url) {
   .then(resp => {
     sending = false;
 
+    // Response depends on the conditions:
+    // 1. URL is not accessible publicly.
+    // 2. Not eonugh paragraphs/text is extracted from the HTML.
+    // 3. Genuine server error.
     if (resp && resp.data && resp.data.status === 'success') {
       chrome.browserAction.setBadgeBackgroundColor({ color: 'rgba(0,0,0,0)' });
       chrome.browserAction.setBadgeText({text: `sav`});
@@ -75,11 +80,17 @@ function saveUrl(url) {
     return {};
   })
   .catch(err => {
-    // Proceed to the next regardless.
+    // Proceed to the next regardless without handling the error in the frontend.
     sending = false;
+
+    return {};
   });
 }
 
+// tabCount checks how long the current url is being active for.
+const tabCount = {};
+
+// renewTab tracks which site is currently active and for how long.
 function renewTab(url) {
 
   if (!url) {
@@ -101,9 +112,8 @@ function renewTab(url) {
   tabCount[url] = 1;
 }
 
-// Check reading every 5 seconds.
+// Check the status of user window every 5 seconds to determine if the reading is happening.
 const timeInterval = 5000;
-const tabCount = {};
 setInterval(() => {
   if (!isUserLoggedin) return;
 
@@ -121,8 +131,8 @@ setInterval(() => {
       return;
     }
 
-    // Need to access window to cehck the user is actually on the page, not on other apps.
-    // Put the check inside tab query to reset url timer (window has no access to url).
+    // Need to access window to cehck the user is actually on the Chrome page, not on other apps.
+    // Put the check inside tab query to reset url timer (Chrome windows has no access to url).
     chrome.windows.getCurrent(null, (wind) => {
       if (!wind || !wind.focused) {
         return;
@@ -136,7 +146,7 @@ setInterval(() => {
         return;
       }
 
-      // Number times timeInterval is the time required for the window to be active before being saved.
+      // durationRequiredToSave is the number requirement for the window to be active before prompting to save.
       const durationRequiredToSave = 5;
       if (tabCount[tabUrl] !== 5) {
         // Keep counting up.
@@ -165,7 +175,6 @@ function getUser() {
     return user;
   })
   .catch(err => {
-    console.log('err', err);
     return null;
   });
 }
@@ -208,24 +217,17 @@ setInterval(() => {
   notifyUserToLogin();
 }, userCheckInterval);
 
-// Keep this listener so background script stays active. Chrome extension needs at least one listener.
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-});
-
 // Reset the badge every time tab is navigated to a different page.
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+  // If user is not logged in, keep that notification badge.
   if (!isUserLoggedin) return;
 
   chrome.browserAction.setBadgeText({text: ``});
 });
 
-// Open web page for the first time.
+// Ask user to signup or login when the extension is installed the first time. Ignore the update.
 chrome.runtime.onInstalled.addListener(object => {
   if (object.reason === 'install') {
-    // COMMENT: No popup without proper landing page. The site asks the user to install the extension agian and it's not helpful.
-    // chrome.tabs.create({url: "https://kaffae.com"}, () => {
-    //     console.log("New tab launched with https://kaffae.com");
-    // });
 
     return notifyUserToLogin();
   }
